9b04ebe2c9db251d1d18d93abe8888f7cf9ea9b9
ThiagoLp <thiagoluiz.ps@hotmail.com>
2022-02-27 16:33:59 -0300

    add music and music button

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+// ***** This control the music
+const musciPauseBtn = document.querySelector('#music-pause-btn')
+const music = document.querySelector('#audio')
+music.play()
+music.volume = 0.3
+let musicIsPlaying = true
+
+function musicTogglePause() {
+  if (musicIsPlaying) {
+    music.pause()
+    musicIsPlaying = false
+    musciPauseBtn.classList.toggle('active')
+  } else {
+    music.play()
+    musicIsPlaying = true
+    musciPauseBtn.classList.toggle('active')
+  }
+}
+
+musciPauseBtn.addEventListener('click', musicTogglePause)
+// *********************************************************
+
 const navMenu = document.querySelector('#nav')
 const menuBtn = document.querySelector('#menu-hamburguer')
 
